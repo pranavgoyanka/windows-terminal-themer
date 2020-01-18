3c2b487909e470c31ded1e99018e99157d88af26
Pranav Goyanka <pgoyanka@gmail.com>
2020-01-18 21:57:55 +0530

    Create a unified function for writing all attributes

--- gui.py
+++ gui.py
@@ -3,13 +3,17 @@ from tkinter import *
 
 root = Tk()
 
-def acryOpcty():
-    th.currentProf['acrylicOpacity'] = opcty.get()
-    th.writeProfile(th.currentProf)
+# def acryOpcty():
+#     th.currentProf['acrylicOpacity'] = opcty.get()
+#     th.writeProfile(th.currentProf)
 
-def cursorShape():
-    th.currentProf['cursorShape'] = str(curshape.get())
-    # print(th.currentProf)
+# def cursorShape():
+#     th.currentProf['cursorShape'] = str(curshape.get())
+#     # print(th.currentProf)
+#     th.writeProfile(th.currentProf)
+
+def unifiedWrite(profName, val):
+    th.currentProf[profName] = val
     th.writeProfile(th.currentProf)
 
 # Define variables
@@ -28,18 +32,20 @@ cursorShapeList = [
 LabelFrame(root, text='Windows Terminal Themer',padx=5, pady=6).grid(padx=5, pady=5)
 
 # Acrylic Opacity
+Label(root, text='Acrylic Opacity').grid()
 acryOpctyScale = Scale(root, variable = opcty, orient = HORIZONTAL, from_ = 0, resolution = 0.01, to = 1.0)
 acryOpctyScale.grid(row = 0, column = 0)
-acryOpctyBttn = Button(root, text = 'Set', command=acryOpcty)
+acryOpctyBttn = Button(root, text = 'Set', command=lambda: unifiedWrite('acrylicOpacity', opcty.get()))
 acryOpctyBttn.grid(row= 0, column = 1)
 
 # Cursor Shape
 Label(root, text='Cursor Shape').grid()
 for i in range(len(cursorShapeList)):
-    R = Radiobutton(root, text=cursorShapeList[i], variable=curshape, value=cursorShapeList[i], command=cursorShape)
+    R = Radiobutton(root, text=cursorShapeList[i], variable=curshape, value=cursorShapeList[i], command=lambda: unifiedWrite('cursorShape', curshape.get()))
     R.grid()
     
-
+# Background Color
+Label(root, text='Cursor Shape').grid()
 
 
 root.mainloop()
